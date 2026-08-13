852f3497a4b24c2255be9a07f4bf4f6fd13a46a3
ghskyzhou <zhou_sky@outlook.com>
2026-08-14 05:30:06 +0800

    20260814 Polygon Update

--- include/polygon.hpp
+++ include/polygon.hpp
@@ -10,6 +10,7 @@ Lib with Polygon.
 
 namespace geo {
     struct Polygon {
+        // Strictly Counter Clockwise
         std::vector <Point> p;
 
         Polygon() = default;
@@ -58,13 +59,47 @@ namespace geo {
     };
 
     inline Real signedArea(const Polygon& p) {
+        int n = p.size();
         Real re = 0;
-        for(int i = 0; i < p.size(); i++) re += cross(p[i], p[(i+1)%p.size()]);
+        for(int i = 0; i < n; i++) re += cross(p[i], p[(i+1)%n]);
         return re / 2.0;
         // Shoelace Formula
     }
 
     inline Real area(const Polygon& p) {
         return std::abs(signedArea(p));
+    }
+
+    inline void makeCCW(Polygon& p) {
+        // Switch Clockwise polygon to Counter Clockwise
+        if(sign(signedArea(p)) < 0) {
+            std::reverse(p.begin(), p.end());
+        }
+    }
+
+    inline bool isConvex(const Polygon& p) {
+        int n = p.size();
+        for(int i = 0; i < n-1; i++) {
+            if(orient(p[i], p[i+1], p[(i+2)%n]) == -1) return false;
+        }
+        return true;
+    }
+
+    inline std::istream& operator>>(std::istream& in, Polygon& poly) {
+        int n;
+        in >> n;
+        poly.p.resize(n);
+        for(int i = 0; i < n; i++) {
+            in >> poly[i];
+        }
+        return in;
+    }
+
+    inline std::ostream& operator<<(std::ostream& out, const Polygon& poly) {
+        out << poly.size() << '\n';
+        for(Point p : poly) {
+            out << p << '\n';
+        }
+        return out;
     }
 }
